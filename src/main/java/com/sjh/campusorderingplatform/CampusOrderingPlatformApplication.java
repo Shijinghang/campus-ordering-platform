8e5b99a2891f4170a0126307139e2bc0292a20bc
sjh <sjh@example.com>
2022-11-05 17:57:37 +0800

    first push

---
+++ src/main/java/com/sjh/campusorderingplatform/CampusOrderingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.campusorderingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOrderingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusOrderingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjh/campusorderingplatform/CampusOrderingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.campusorderingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusOrderingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
